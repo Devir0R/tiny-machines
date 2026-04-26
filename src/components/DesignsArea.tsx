bce004f1e0ac5b3753bde5c19c7abd43d0393051
Dvir Raviv <devir0raviv@gmail.com>
2026-04-26 14:04:50 +0300

    added comment about cashing in designs

--- src/components/DesignsArea.tsx
+++ src/components/DesignsArea.tsx
@@ -13,6 +13,7 @@ export const DesignsArea = ({ designs, onMachineClick, calculateDesignPotentialS
   return (
     <div className="design-area col-span-1 p-4 text-center flex flex-col items-center w-full rounded-sm">
       <h2 className="text-xl font-bold mb-1">Designs</h2>
+      <h3 className="text-sm font-bold mb-1">you may cash in a design for one of the machines on it</h3>
       <div className="interactable-area w-full max-w-[60vw] h-[40vh] outline-2 outline-dashed outline-gray-400 p-4 rounded-lg overflow-x-auto">
         <div className="h-full grid grid-cols-5 gap-4 items-stretch">
           {designs.map((design, index) => (
